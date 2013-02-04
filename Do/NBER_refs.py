f9f08e7e56fde2550810d00224dbf41a677c8529
Damian C. Clarke <damian.clarke@economics.ox.ac.uk>
2013-02-04 12:26:28 -0300

    NBER refs working well, but not yet stripping out years

--- Do/NBER_refs.py
+++ Do/NBER_refs.py
@@ -16,6 +16,9 @@
 import os
 import re
 
+NoRefs = open('NoRefs.txt', 'w')
+
+
 JEL = "ABCDEFGHIJKLMNOPQRZ"
 for x in JEL:
 	if not os.path.exists("../refs"): # make files to save biblio txts
@@ -23,29 +26,29 @@ for x in JEL:
 	if not os.path.exists("../refs/" + x): # make files to save biblio txts
 		os.makedirs("../refs/" + x)
 
-d = {}
-for line1 in open('JEL_codes.txt'):
-	dictname = '../refs/' + line1[0:1] + '/Result' + line1[0:2] + '.txt'
-	d[line1[0:2]] = dictname
+for y in range(2000,2014):	
+	for line1 in open('JEL_codes.txt'):
+		filename = '../refs/' + line1[0:1] + '/Result' + line1[0:2] + str(y) + '.txt'
+		key = open(filename, 'w')
+	
+		#***************************************************************************
+		# (2) Strip data from bibliography
+		#***************************************************************************
+		for line in open("NBER_papers.txt"):
+			subfolder = line[0:2]
+			folder = line[0:1]
+			p = re.findall('w[0-9]+', line)	
+			year = line[-11:-7]
+			name = 'Result' + subfolder
+			for a in p:
+				papercode = a
 
+			references = None
+			References = None
+			REFERENCES = None
 
-for key, value in d.iteritems():
-	key = open(value, 'w')
-
-	#***************************************************************************
-	# (2) Strip data from bibliography
-	#***************************************************************************
-	for line in open("NBER_papers.txt"):
-		subfolder = line[0:2]
-		folder = line[0:1]
-		p = re.findall('w[0-9]+', line)	
-		year = line[-11:-7]
-		name = 'Result' + subfolder
-		for a in p:
-			papercode = a
-
-		for y in range(2000,2014):	
-			if str(y) == str(year) and line1[0:2]==line[0:2]:
+			if line1[0:2]==line[0:2] and str(y) == str(year):
+#				print papercode, folder, subfolder
 				txtpaper = "../" + "pdfs" + "/" + folder + "/" + subfolder + "/" + papercode + ".txt"
 				txtpaperread = open(txtpaper, 'r').read()
 	
@@ -53,24 +56,61 @@ for key, value in d.iteritems():
 				# I just have to make these reasonably poorly stripped bibliographies be
 				# written into the files ResultA0, ResultA1.  make a name of 
 				#***************************************************************
-				references = None
-				References = None
-				REFERENCES = None
-				
-				references = re.search('\b references', txtpaperread)
-				if references!=None:
-					refposition=references.start()
+#				references = re.search('\b references', txtpaperread)
+#				if references!=None:
+#					refposition=references.start()
 				References = re.search('References', txtpaperread)
 				if References!=None:
 					Refposition = References.start()
-#					print txtpaperread[Refposition:Refposition+5000]
-					key.write(txtpaperread[Refposition:Refposition+5000] + "\n")
+					key.write("PAPERNUMBER=" + papercode + subfolder + "\n")
+					key.write(txtpaperread[Refposition:Refposition+10000] + "\n")
 				REFERENCES = re.search('REFERENCES', txtpaperread)
 				if REFERENCES!=None:
 					REFposition = REFERENCES.start()
-					key.write(txtpaperread[REFposition:REFposition+5000] + "\n")
-				
-				if REFERENCES==None and References==None:
+					key.write(txtpaperread[REFposition:REFposition+10000] + "\n")
+				REF2 = re.search('R EFERENCES', txtpaperread)
+				if REF2!=None:
+					REF2position = REF2.start()
+					key.write(txtpaperread[REF2position:REF2position+10000] + "\n")					
+				REF3 = re.search('Bibliography', txtpaperread)
+				if REF3!=None:
+					REF3position = REF3.start()
+					key.write(txtpaperread[REF3position:REF3position+10000] + "\n")					
+				REF4 = re.search('Referemces', txtpaperread)
+				if REF4!=None:
+					REF4position = REF4.start()
+					key.write(txtpaperread[REF4position:REF4position+10000] + "\n")					
+				REF5 = re.search('Works Cited', txtpaperread)
+				if REF5!=None:
+					REF5position = REF5.start()
+					key.write(txtpaperread[REF5position:REF5position+10000] + "\n")					
+				REF6 = re.search('Literature Cited', txtpaperread)
+				if REF6!=None:
+					REF6position = REF6.start()
+					key.write(txtpaperread[REF6position:REF6position+10000] + "\n")					
+				REF7 = re.search('Literature cited', txtpaperread)
+				if REF7!=None:
+					REF6position = REF7.start()
+					key.write(txtpaperread[REF7position:REF7position+10000] + "\n")					
+				REF8 = re.search('Further Reading', txtpaperread)
+				if REF8!=None:
+					REF8position = REF8.start()
+					key.write(txtpaperread[REF8position:REF8position+10000] + "\n")					
+				REF9 = re.search('WORKS CITED', txtpaperread)
+				if REF9!=None:
+					REF9position = REF9.start()
+					key.write(txtpaperread[REF9position:REF9position+10000] + "\n")					
+
+
+
+
+
+
+				if REFERENCES==None and References==None and REF2==None and REF3==None and REF4==None and REF5==None and REF6==None and REF7==None and REF8==None and REF9==None:
 					print subfolder, papercode
+					NoRefs.write(subfolder + '\t' + papercode + '\n')
+
 	
-	key.close()
+
+		key.close()
+NoRefs.close()
